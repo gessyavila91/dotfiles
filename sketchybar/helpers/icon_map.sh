36983ff1403e060521558161b5aedac5a1577a72
gessyavila91 <gessyavila91@gmail.com>
2024-11-18 21:39:12 -0600

    implement SoichiroYamane dotfiles

--- sketchybar/helpers/icon_map.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/kvndrsslr/sketchybar-app-font
-
-source ./path/to/icon_map.sh
-
-__icon_map "${app_name}"
-symbol_ligature="${icon_result}"
--- sketchybar/plugins/new_space.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-yabai -m space --create && \
-    index="$(yabai -m query --spaces --display | jq 'map(select(."is-native-fullscreen" == false))[-1].index')" && \
-    yabai -m space --focus "${index}"
